12adca4fdc4d95df04e871ad1a21e41a44b6a42b
Diego_Maia <diego.trab202@gmail.com>
2025-06-03 12:56:46 -0300

    Inserção de dois exercícios respondidos

---
+++ Exercício_2.py
@@ -0,0 +1,29 @@
+
+#! Exercício 2.1
+"""
+1 -> Vimos que n = 42 é legal. E 42 = n?
+R.: É ilegal, devido a posição do numeral que está a esquerda da variável. 42 = n retorna um error de Sintaxe (SyntaxError).
+
+2 -> Ou x = y = 1
+R.: x = y = 1 é legal, no Python diferentes variáveis podem possuir um mesmo valor.
+
+3 -> Em algumas linguagens, cada instrução termina em um ponto vírgula ;. O que acontece se você puser um ponto e vírgula no fim
+de uma instrução em Python?
+R.: Nada, a instrução funciona.
+
+4 -> E Se puser um ponto no fim de uma instrução
+R.: Ao inserir um ponto no fim de uma instrução o Python retorna um erro de sintaxe (SyntaxError).
+
+5 -> Em notação matemática é possível multiplicar x e y desta forma: XY. O que acontece se você tentar fazer o mesmo no Python?
+R.: Ao tentar realizar a multiplicação do XY sem o operador responsável presente, o Python retorna um erro de nome (NameError).
+
+"""
+
+#! Exercício 2.2 -- Python como calculadora
+"""
+1 -> O volume de uma esfera com raio r é 4/3*pi*r**3.
+Qual é o volume de uma esfera com o raio 5?
+R.: 4/3* 3.14 * 5**3 = 523.333333333334
+
+
+"""
